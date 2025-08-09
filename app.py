906857a1b0a6e106f9aaefcdd519deb0b5b902ab
unknown <samra.infosec@gmail.com>
2025-08-10 00:13:52 +0500

    Updated headings text

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    site_title = os.getenv("SITE_TITLE", "Default Site")
+    site_title = os.getenv("SITE_TITLE", "Cloud Test App")
     return render_template("home.html", site_title=site_title)
 
 @app.route('/about')
 def about():
-    site_title = os.getenv("SITE_TITLE", "Default Site")
+    site_title = os.getenv("SITE_TITLE", "Cloud Test App")
     return render_template("about.html", site_title=site_title)
 
 if __name__ == '__main__':
